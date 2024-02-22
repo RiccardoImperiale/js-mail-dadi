f4ecabd4853c23bb0b0f50e94a8a4ec427005bb6
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-02-22 14:27:38 +0100

    generate random numbers for the player and the computer

--- assets/main.js
+++ assets/main.js
@@ -1,27 +1,27 @@
 const form = document.querySelector('form');
 const inputEmail = document.querySelector('#email');
 const emailHelp = document.querySelector('#emailHelp');
-
 const authUsers = ['ri@chi.com', 'richi@123.it'];
-
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     // user email
     const userEmail = inputEmail.value;
-    console.log(userEmail);
     // check if user email is already present in the list of who can access
     for (let i = 0; i < authUsers.length; i++) {
         if (userEmail === authUsers[i]) {
             emailHelp.innerText = 'You are an authorized user';
             emailHelp.classList.add('text-success');
-            console.log('yes');
-            return;
+            break;
         } else {
             emailHelp.innerText = 'You are NOT an authorized user';
             emailHelp.classList.add('text-danger');
-            console.log('no');
-            return;
+            break;
         }
     }
 })
+
+// dice game
+const playerRandNum = Math.floor(Math.random() * 6) + 1;
+const computerRandNum = Math.floor(Math.random() * 6) + 1;
+console.log(playerRandNum, computerRandNum);
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 - Check if it is already present in the list of who can access
 - Print an appropriate message regarding the outcome of the check
 
-## Gioco dei dadi
+## Dice Game
 - Generate a random number from 1 to 6, for both the player and the computer
 - Determine the winner, based on who has the highest score
 
