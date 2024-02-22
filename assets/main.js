b4f53673df7f1326cf890a8b01aedbf2cbcc33f1
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-02-22 14:21:59 +0100

    add check if the user is authorized

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,8 @@
 const form = document.querySelector('form');
 const inputEmail = document.querySelector('#email');
+const emailHelp = document.querySelector('#emailHelp');
+
+const authUsers = ['ri@chi.com', 'richi@123.it'];
 
 
 form.addEventListener('submit', (e) => {
@@ -8,5 +11,17 @@ form.addEventListener('submit', (e) => {
     const userEmail = inputEmail.value;
     console.log(userEmail);
     // check if user email is already present in the list of who can access
-    
+    for (let i = 0; i < authUsers.length; i++) {
+        if (userEmail === authUsers[i]) {
+            emailHelp.innerText = 'You are an authorized user';
+            emailHelp.classList.add('text-success');
+            console.log('yes');
+            return;
+        } else {
+            emailHelp.innerText = 'You are NOT an authorized user';
+            emailHelp.classList.add('text-danger');
+            console.log('no');
+            return;
+        }
+    }
 })
