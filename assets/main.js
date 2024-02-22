37a1a10dee714f2f82869a5d8c728a0e308e984f
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-02-22 14:04:25 +0100

    add ask user email

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,12 @@
-console.log('okkkkk');
+const form = document.querySelector('form');
+const inputEmail = document.querySelector('#email');
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // user email
+    const userEmail = inputEmail.value;
+    console.log(userEmail);
+    // check if user email is already present in the list of who can access
+    
+})
