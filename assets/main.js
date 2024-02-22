64789e2d3d8082f2bc2a0d9910f8c31f372e2b2b
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-02-22 19:14:04 +0100

    fix authorized email validation

--- assets/main.js
+++ assets/main.js
@@ -2,22 +2,26 @@ const form = document.querySelector('form');
 const inputEmail = document.querySelector('#email');
 const emailHelp = document.querySelector('#emailHelp');
 const authUsers = ['ri@chi.com', 'richi@123.it'];
+let isAuthUser = false;
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    // user email
     const userEmail = inputEmail.value;
     // check if user email is already present in the list of who can access
     for (let i = 0; i < authUsers.length; i++) {
         if (userEmail === authUsers[i]) {
-            emailHelp.innerText = 'You are an authorized user';
-            emailHelp.classList.add('text-success');
-            break;
-        } else {
-            emailHelp.innerText = 'You are NOT an authorized user';
-            emailHelp.classList.add('text-danger');
-            break;
-        }
+            isAuthUser = true; 
+        } 
+    }
+
+    if (isAuthUser) {
+        emailHelp.innerText = 'You are an authorized user';
+        emailHelp.classList.remove('text-danger');
+        emailHelp.classList.add('text-success');
+    } else {
+        emailHelp.innerText = 'You are NOT an authorized user';
+        emailHelp.classList.remove('text-success');
+        emailHelp.classList.add('text-danger');
     }
 })
 
